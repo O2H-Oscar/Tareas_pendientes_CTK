39f8beaddceca002768bc542112e7a3f720993fc
O2H-Oscar <oohenandez91@gmail.com>
2025-04-13 02:20:43 +0200

    Update Tareas_pendientes_CTK.py

--- Tareas_pendientes_CTK.py
+++ Tareas_pendientes_CTK.py
@@ -36,7 +36,54 @@ root.title("Tareas pendientes")
 root.resizable(False,False)
 icon_path = resource_path("tareas.ico")
 root.iconbitmap(icon_path)
-ctk.set_appearance_mode("light")
+ctk.set_appearance_mode("light") 
+
+
+# Frames
+
+
+frame_superior=ctk.CTkFrame(root,fg_color="light blue",height = 240)
+frame_superior.pack(side="top",fill="both")
+frame_superior.pack_propagate(False)  # Evita que el frame cambie de tamaño
+
+frame_interior=ctk.CTkFrame(frame_superior,fg_color="light blue",height = 80,width=560)
+frame_interior.pack(side="top",padx=5,pady=5)
+frame_interior.pack_propagate(False)  # Evita que el frame cambie de tamaño
+
+frame_inferior=ctk.CTkFrame(root,fg_color="light blue")
+frame_inferior.pack(side="bottom",fill="both",expand="True")
+frame_inferior.pack_propagate(False)  # Evita que el frame cambie de tamaño
+
+
+# Etiquetas
+
+etiqueta_fecha=ctk.CTkLabel(frame_interior,text="Fecha",font=("Arial",14,'bold'))
+etiqueta_fecha.pack(side="left",padx=(32,0),pady=(30,10))
+
+# Entradas
+
+entrada_fecha=ctk.CTkEntry(frame_interior,placeholder_text="DD-MM-YYYY",width=80,font=("Arial",10,'bold'))
+entrada_fecha.pack(side="left",padx=(2,0),pady=(30,10))
+
+
+
+
+#Botones
+
+
+
+#Treeview
+
+
+
+
+# Llamada de funciones
+
+
+
+
+
+
 
 root.mainloop()
 
